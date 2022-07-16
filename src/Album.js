af6cde2747ca6a3e3e5f872448644c22fc9e10b7
oslook <oslook@users.noreply.github.com>
2022-07-16 08:06:08 +0000

    3

--- src/Album.js
+++ src/Album.js
@@ -106,7 +106,7 @@ export default function Album() {
           {/* End hero unit */}
           <Grid container spacing={2}>
             {itemData.map((item) => (
-              <Grid item key={item} xs={12} sm={6} md={3}>
+              <Grid item key={item} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
