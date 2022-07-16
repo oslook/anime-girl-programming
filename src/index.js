2b527abc30c63ecebd791a4634b6ae932fdc5c7c
oslook <oslook@users.noreply.github.com>
2022-07-16 07:14:47 +0000

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 // import App from './App';
+import Album from './Album'
 import theme from './theme';
 
 const rootElement = document.getElementById('root');
