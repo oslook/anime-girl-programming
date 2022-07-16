e0b58ed6884618fc3d074d9fe1055e05bb3625dc
oslook <oslook@users.noreply.github.com>
2022-07-16 07:07:20 +0000

    album

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
+import Album from './Album';
 
 function Copyright() {
   return (
@@ -23,9 +24,10 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example
+          Programning Girls
         </Typography>
         <ProTip />
+        <Album />
         <Copyright />
       </Box>
     </Container>
